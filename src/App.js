ecb61519ab505fb74da4f0d2cbcae7772296cc71
arulprofessional <106566414+arulprofessional@users.noreply.github.com>
2023-02-09 21:28:15 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 import AllContactsPage from './pages/AllContacts';
 import NewContactPage from './pages/NewContact';
@@ -11,12 +11,15 @@ import MainNavigation from './components/layout/MainNavigation';
 export default function App() {
   return (
     <div>
-      <MainNavigation />
-      <Routes>
-        <Route path="/" exact element={<AllContactsPage />} />
-        <Route path="new-contact" exact element={<NewContactPage />} />
-        <Route path="favorites" exact element={<FavoritesPage />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainNavigation />}>
+            <Route index element={<AllContactsPage />} />
+            <Route path="new-contact" exact element={<NewContactPage />} />
+            <Route path="favorites" exact element={<FavoritesPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import '../../style.css';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 
 export default function MainNavigation() {
   return (
-    <header className={classes.header}>
-      <div>Add and Fetch Records using React and Firebase</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">All Contacts</Link>
-          </li>
-          <li>
-            <Link to="/new-contact">Add New Contact</Link>
-          </li>
-          <li>
-            <Link to="/favorites">My Favorites</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className={classes.header}>
+        <div>Add and Fetch Records using React and Firebase</div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">All Contacts</Link>
+            </li>
+            <li>
+              <Link to="/new-contact">Add New Contact</Link>
+            </li>
+            <li>
+              <Link to="/favorites">My Favorites</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+    </>
   );
 }
