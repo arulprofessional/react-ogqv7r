310c37c8df05b3eee812874dd8e4ca1984728295
Arul Prakash Sangararaj <arulprofessional@gmail.com>
2023-02-14 14:39:40 +0000

    Update 9 files

--- src/components/contact/ContactItem.js
+++ src/components/contact/ContactItem.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import '../../style.css';
+import React from "react";
+import "../../style.css";
+
+import classes from "./ContactItem.module.css";
+import Card from "../ui/Card";
 
 export default function ContactItem(props) {
   return (
-    <li key={props.id}>
-      <div>
-        <h3>{props.data.title}</h3>
-        <span>{props.data.name}</span>
-        <span>{props.data.mobileNumber}</span>
-        <span>{props.data.email}</span>
-        <address>{props.data.address}</address>
-        <p>{props.data.description}</p>
-      </div>
+    <li key={props.id} className={classes.item}>
+      <Card>
+        <div className={classes.item}>
+          <h3>{props.data.title}</h3>
+          <h4>{props.data.name}</h4>
+          <h4>{props.data.mobileNumber}</h4>
+          <h4>{props.data.email}</h4>
+          <address>{props.data.address}</address>
+          <p>{props.data.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
