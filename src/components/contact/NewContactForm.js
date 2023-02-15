17552adca02b97de4c212f17e4bd4f8ba55b4366
Arul Prakash Sangararaj <arulprofessional@gmail.com>
2023-02-15 14:24:54 +0000

    useNavigate useEffect Add new contact in Firebase Fetching records from Firebase

---
+++ src/components/contact/NewContactForm.js
@@ -0,0 +1,69 @@
+import { useRef } from "react";
+import Card from "../ui/Card";
+import classes from "./NewContactForm.module.css";
+
+export default function NewContacForm(props) {
+  const titleInputRef = useRef();
+  const nameInputRef = useRef();
+  const phoneNumberInputRef = useRef();
+  const emailInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const contacData = {
+      title: titleInputRef.current.value,
+      name: nameInputRef.current.value,
+      phoneNumber: phoneNumberInputRef.current.value,
+      email: emailInputRef.current.value,
+      address: addressInputRef.current.value,
+      description: descriptionInputRef.current.value,
+    };
+    props.onAddContact(contacData);
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" required ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" required ref={nameInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input
+            type="text"
+            id="phoneNumber"
+            required
+            ref={phoneNumberInputRef}
+          />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="email">E-mail</label>
+          <input type="text" id="email" required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" id="address" required ref={addressInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionInputRef}
+          ></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Contact</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
--- src/pages/AllContacts.js
+++ src/pages/AllContacts.js
@@ -1,36 +1,48 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "../style.css";
 
 import ContactList from "../components/contact/ContactList";
 
 import Layout from "../components/layout/Layout";
 
-const DUMMY_DATA = [
-  {
-    id: "c1",
-    title: "DUMMY_DATA 1",
-    name: "Name 1",
-    mobileNumber: "0123456789",
-    email: "email1@gmail.com",
-    address: "Address 1",
-    description: "Description 1",
-  },
-  {
-    id: "c2",
-    title: "DUMMY_DATA 2",
-    name: "Name 2",
-    mobileNumber: "0123456789",
-    email: "email2@gmail.com",
-    address: "Address 2",
-    description: "Description 2",
-  },
-];
-
 export default function AllContactsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactList, setContactList] = useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-ccd14-default-rtdb.firebaseio.com/contacts.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const contacts = [];
+
+        for (const key in data) {
+          const contact = {
+            id: key,
+            ...data[key],
+          };
+          contacts.push(contact);
+        }
+        setIsLoading(false);
+        setContactList(contacts);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <Layout>
       <h1>All Contacts Page</h1>
-      <ContactList items={DUMMY_DATA} />
+      <ContactList items={contactList} />
     </Layout>
   );
 }
